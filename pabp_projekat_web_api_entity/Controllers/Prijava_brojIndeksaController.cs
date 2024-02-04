a225260e9a18cdd30e725b86331f79a10ed93144
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2024-02-04 11:41:40 +0100

    Prijava_brojIndeksa kontroler

---
+++ pabp_projekat_web_api_entity/Controllers/Prijava_brojIndeksaController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pabp_projekat_web_api_entity.Models;
+
+namespace pabp_projekat_web_api_entity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Prijava_brojIndeksaController : ControllerBase
+    {
+        private readonly MasterContext _context;
+
+        public Prijava_brojIndeksaController(MasterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Prijava_brojIndeksa
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Prijava_brojIndeksa>>> GetPrijavas()
+        {
+            return await _context.Prijavas.ToListAsync();
+        }
+
+        [HttpGet("student/{id}")]
+        public async Task<ActionResult<IEnumerable<Prijava_brojIndeksa>>> GetPrijava_brojIndeksaZaStudenta(int id)
+        {
+            var prijava_brojIndeksa = await _context.Prijavas.Where(p => p.IdStudenta == id).ToListAsync();
+
+            if (prijava_brojIndeksa == null)
+            {
+                return NotFound();
+            }
+
+            return prijava_brojIndeksa;
+        }
+        [HttpGet("ispit/{id}")]
+        public async Task<ActionResult<IEnumerable<Prijava_brojIndeksa>>> GetPrijava_brojIndeksaZaIspit(int id)
+        {
+            var prijava_brojIndeksa = await _context.Prijavas.Where(p => p.IdIspita == id).ToListAsync();
+
+            if (prijava_brojIndeksa == null)
+            {
+                return NotFound();
+            }
+
+            return prijava_brojIndeksa;
+        }
+
+
+        // GET: api/Prijava_brojIndeksa/5
+        /*[HttpGet("{id}")]
+        public async Task<ActionResult<Prijava_brojIndeksa>> GetPrijava_brojIndeksa(int id)
+        {
+            var prijava_brojIndeksa = await _context.Prijavas.FindAsync(id);
+
+            if (prijava_brojIndeksa == null)
+            {
+                return NotFound();
+            }
+
+            return prijava_brojIndeksa;
+        }*/
+
+        // PUT: api/Prijava_brojIndeksa/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /*[HttpPut("{id}")]
+        public async Task<IActionResult> PutPrijava_brojIndeksa(int id, Prijava_brojIndeksa prijava_brojIndeksa)
+        {
+            if (id != prijava_brojIndeksa.IdStudenta)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(prijava_brojIndeksa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Prijava_brojIndeksaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }*/
+
+        // POST: api/Prijava_brojIndeksa
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Prijava_brojIndeksa>> PostPrijava_brojIndeksa(Prijava_brojIndeksa prijava_brojIndeksa)
+        {
+            _context.Prijavas.Add(prijava_brojIndeksa);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (Prijava_brojIndeksaExists(prijava_brojIndeksa.IdStudenta))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // DELETE: api/Prijava_brojIndeksa/5
+        /*[HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePrijava_brojIndeksa(int id)
+        {
+            var prijava_brojIndeksa = await _context.Prijavas.FindAsync(id);
+            if (prijava_brojIndeksa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Prijavas.Remove(prijava_brojIndeksa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }*/
+
+        private bool Prijava_brojIndeksaExists(int id)
+        {
+            return _context.Prijavas.Any(e => e.IdStudenta == id);
+        }
+    }
+}
--- pabp_projekat_web_api_entity/Models/MasterContext.cs
+++ pabp_projekat_web_api_entity/Models/MasterContext.cs
@@ -29,6 +29,8 @@ public partial class MasterContext : DbContext
 
     public virtual DbSet<Zapisnik> Zapisniks { get; set; }
 
+    public virtual DbSet<Prijava_brojIndeksa> Prijavas { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=BEOKROS\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
