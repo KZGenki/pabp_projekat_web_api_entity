6c1d6af6e12a79a4e1111dbf7c21c787a2c220b2
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2024-02-04 11:10:34 +0100

    Zapisniks kontroler

---
+++ pabp_projekat_web_api_entity/Controllers/ZapisniksController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pabp_projekat_web_api_entity.Models;
+
+namespace pabp_projekat_web_api_entity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZapisniksController : ControllerBase
+    {
+        private readonly MasterContext _context;
+
+        public ZapisniksController(MasterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Zapisniks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Zapisnik>>> GetZapisniks()
+        {
+            return await _context.Zapisniks.ToListAsync();
+        }
+
+        // GET: api/Zapisniks/5
+        /*[HttpGet("{id}")]
+        public async Task<ActionResult<Zapisnik>> GetZapisnik(int id)
+        {
+            var zapisnik = await _context.Zapisniks.FindAsync(id);
+
+            if (zapisnik == null)
+            {
+                return NotFound();
+            }
+
+            return zapisnik;
+        }*/
+        [HttpGet("{idStudenta}/{idIspita}")]
+        public async Task<ActionResult<Zapisnik>> GetZapisnik(short idStudenta, short idIspita)
+        {
+            var zapisnik = await _context.Zapisniks
+                .Where(z => z.IdStudenta == idStudenta && z.IdIspita == idIspita)
+                .FirstOrDefaultAsync();
+
+            if (zapisnik == null)
+            {
+                return NotFound();
+            }
+
+            return zapisnik;
+        }
+        [HttpGet("Student/{idStudenta}")]
+        public async Task<ActionResult<IEnumerable<Zapisnik>>> GetZapisnikStudent(short idStudenta)
+        {
+            var zapisniks = await _context.Zapisniks
+                .Where(z => z.IdStudenta == idStudenta)
+                .ToListAsync();
+
+            if (zapisniks == null)
+            {
+                return NotFound();
+            }
+
+            return zapisniks;
+        }
+        [HttpGet("Ispit/{idIspita}")]
+        public async Task<ActionResult<IEnumerable<Zapisnik>>> GetZapisnikIspit(short idIspita)
+        {
+            var zapisniks = await _context.Zapisniks
+                .Where(z => z.IdIspita == idIspita)
+                .ToListAsync();
+
+            if (zapisniks == null)
+            {
+                return NotFound();
+            }
+
+            return zapisniks;
+        }
+        [HttpGet("Polozeni/{id}")]
+        public async Task<ActionResult<object>> GetPolozeni(short id)
+        {
+            var zapisnik = await _context.Zapisniks
+                .Where(z => z.IdStudenta == id && z.Ocena > 5)
+                .ToListAsync();
+
+            List<int> ispitiId = new List<int>();
+            List<object> ocene = new List<object>();
+
+            double suma = 0;
+            foreach (var item in zapisnik)
+            {
+                ispitiId.Add(item.IdIspita);
+                ocene.Add(new { ocena = item.Ocena, id = item.IdIspita });
+                suma += item.Ocena;
+            }
+            double prosek = suma / ispitiId.Count();
+
+            var ispiti = await _context.Ispits
+                .Where(i => ispitiId.Contains(i.IdIspita))
+                .ToListAsync();
+            var predmetiId = ispiti
+                .Select(i => i.IdPredmeta);
+
+            var Predmeti = await _context.Predmets
+                .Where(p => predmetiId.Contains(p.IdPredmeta))
+                .ToListAsync();
+
+
+
+            return Ok(new
+            {
+                prosecnaOcena = prosek,
+                polozeniPredmeti = Predmeti
+            });
+        }
+
+        // PUT: api/Zapisniks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /*[HttpPut("{id}")]
+        public async Task<IActionResult> PutZapisnik(int id, Zapisnik zapisnik)
+        {
+            if (id != zapisnik.IdStudenta)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(zapisnik).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ZapisnikExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Zapisniks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Zapisnik>> PostZapisnik(Zapisnik zapisnik)
+        {
+            _context.Zapisniks.Add(zapisnik);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ZapisnikExists(zapisnik.IdStudenta))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetZapisnik", new { id = zapisnik.IdStudenta }, zapisnik);
+        }
+
+        // DELETE: api/Zapisniks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteZapisnik(int id)
+        {
+            var zapisnik = await _context.Zapisniks.FindAsync(id);
+            if (zapisnik == null)
+            {
+                return NotFound();
+            }
+
+            _context.Zapisniks.Remove(zapisnik);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }*/
+
+        private bool ZapisnikExists(int id)
+        {
+            return _context.Zapisniks.Any(e => e.IdStudenta == id);
+        }
+    }
+}
